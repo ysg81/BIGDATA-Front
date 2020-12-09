35d02bc1678569c1ed3da4dd0d5ddb5e14d3e904
hchangejae <hcj@payw.org>
2020-12-09 22:45:55 +0900

    영화 흥행률 api 요청 사가 추가

---
+++ src/appSaga.js
@@ -0,0 +1,28 @@
+import {all, call, put, takeEvery} from 'redux-saga/effects';
+
+import { GET_MOVIE_POINT_EXPECT } from './appAction';
+import { apiGet } from './utils/apiUtils';
+
+function* getMoviePointExpect (action) {
+  const {moiveInfo:params} = action.payload;
+
+  try {
+    const r = yield call(apiGet, '/get', params);
+
+    yield put({
+      type: GET_MOVIE_POINT_EXPECT.SUCCESS,
+      result: r,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_MOVIE_POINT_EXPECT.FAILURE,
+      error: e,
+    });
+  }
+}
+
+export default function* rootSaga () {
+  yield all([
+    takeEvery(GET_MOVIE_POINT_EXPECT.REQUEST, getMoviePointExpect),
+  ]);
+}
