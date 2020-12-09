b6d8db1771aedaff6bf9d3cb1660214f130c4370
ysg <qpple6@naver.com>
2020-12-10 00:14:47 +0900

    img change

---
+++ src/components/information/information.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import "./information.css";
+
+class Information extends Component {
+    render(){
+        return(
+            
+            <div className="information">
+                Team N/A
+            </div>
+        );
+    }
+}
+export default Information;
